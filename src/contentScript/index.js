e8f2181a8ad539492e94df6d187ee24333418fce
Lantz <github@muddylemon.com>
2023-11-14 17:22:45 -0600

    wrap function in loader

--- src/contentScript/index.js
+++ src/contentScript/index.js
@@ -8,35 +8,34 @@ const buttonsData = [
   { text: 'lol', title: 'lol 😜' },
   { text: 'rofl', title: 'rofl' },
 ]
-
-const chatInput = document.querySelector('.iogc-ChatPanel-input input.gwt-TextBox')
-const chatButton = document.querySelector('.iogc-ChatPanel-input button')
-
-const chatButtons = document.createElement('div')
-chatButtons.classList.add('iogc-chat-buttons')
-chatButtons.style.display = 'flex'
-chatButtons.style.justifyContent = 'flex-start'
-chatButtons.style.flexWrap = 'wrap'
-
-buttonsData.forEach((buttonData) => {
-  const button = document.createElement('button')
-  button.classList.add('iogc-NewButton-white')
-  button.classList.add('iogc-NewButton')
-  button.classList.add('iogc-ButtonSpace-left')
-  button.style.rightMargin = '5px'
-  button.innerText = buttonData.title
-  button.addEventListener('click', () => {
-    chatInput.value = chatInput.value + ' ' + buttonData.text
-    chatButton.click()
-  })
-  chatButtons.appendChild(button)
-})
-
 const interval = setInterval(() => {
   const chatPanel = document.querySelector('.iogc-ChatPanel')
   if (chatPanel) {
+    const chatInput = document.querySelector('.iogc-ChatPanel-input input.gwt-TextBox')
+    const chatButton = document.querySelector('.iogc-ChatPanel-input button')
+
+    const chatButtonContainer = document.createElement('div')
+    chatButtonContainer.classList.add('iogc-chat-buttons')
+    chatButtonContainer.style.display = 'flex'
+    chatButtonContainer.style.justifyContent = 'flex-start'
+    chatButtonContainer.style.flexWrap = 'wrap'
+
+    buttonsData.forEach((buttonData) => {
+      const button = document.createElement('button')
+      button.classList.add('iogc-NewButton-white')
+      button.classList.add('iogc-NewButton')
+      button.classList.add('iogc-ButtonSpace-left')
+      button.style.rightMargin = '2px'
+      button.innerText = buttonData.title
+      button.addEventListener('click', () => {
+        chatInput.value = `${buttonData.text}`
+        chatButton.click()
+      })
+      chatButtonContainer.appendChild(button)
+    })
+
     console.log("🃏 gpokr's chat panel is loaded")
-    chatPanel.appendChild(chatButtons)
+    chatPanel.appendChild(chatButtonContainer)
     clearInterval(interval)
   }
 }, 100)
